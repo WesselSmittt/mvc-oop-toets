81fddf90a584f066524f12935933b625a281f2d5
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2023-03-14 12:04:08 +0100

    commit

--- app/controllers/Zangeres.php
+++ app/controllers/Zangeres.php
@@ -12,30 +12,34 @@ class Zangeres extends BaseController
     public function index()
     {
         $data = [
-            'title' => 'Overzicht landen van de wereld'
+            'title' => 'Overzicht zangeressen'
         ];
 
         $this->view('zangeres/index', $data);
     }
 
 
-    public function getCountries($id1=NULL, $id2=NULL) 
+    public function getZangeres($id1=NULL, $id2=NULL, $id3=NULL, $id4=NULL, $id5=NULL, $id6=NULL) 
     {
-        $countries = $this->zangeresModel->getCountries();
+        $zangeres = $this->zangeresModel->getZangeres();
 
         $tableRows = "";
-        foreach ($countries as $value) {
+        foreach ($zangeres as $value) {
             $tableRows .= "<tr>
                                 <td>$value->Id</td>
                                 <td>$value->Name</td>
+                                <td>$value->NettoWaarde</td>
+                                <td>$value->Land</td>
+                                <td>$value->Mobiel</td>
+                                <td>$value->Leeftijd</td>
                            </tr>";
         }
 
         $data = [
-            'title' => 'Overzicht landen van Europa',
+            'title' => 'Overzicht zangeressen',
             'tableRows' => $tableRows
         ];
 
-        $this->view('zangeres/getCountries', $data);
+        $this->view('zangeres/getZangeres', $data);
     }
 }
--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -1,3 +1,3 @@
 <h3><?= $data['title']; ?></h3>
 <a href="http://mvc-oop-toets.com/zangeres/index">index</a><br>
-<a href="http://mvc-oop-toets.com/zangeres/getcountries">Alle landen van de wereld</a>
+<a href="http://mvc-oop-toets.com/zangeres/getzangeres">Alle landen van de wereld</a>
