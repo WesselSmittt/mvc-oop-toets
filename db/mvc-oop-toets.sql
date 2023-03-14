748622914481e0ff65633783851c8c522009d253
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2023-03-14 12:47:23 +0100

    Create mvc-oop-toets.sql

---
+++ db/mvc-oop-toets.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 14, 2023 at 11:47 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvc-oop-toets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zangeres`
+--
+
+DROP TABLE IF EXISTS `zangeres`;
+CREATE TABLE IF NOT EXISTS `zangeres` (
+  `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Naam` varchar(100) NOT NULL,
+  `NettoWaarde` smallint(100) NOT NULL,
+  `Land` varchar(100) NOT NULL,
+  `Mobiel` varchar(20) NOT NULL,
+  `Leeftijd` tinyint(3) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `zangeres`
+--
+
+INSERT INTO `zangeres` (`Id`, `Naam`, `NettoWaarde`, `Land`, `Mobiel`, `Leeftijd`) VALUES
+(1, 'Rihanna', 1400, 'Barbados', '+1246 2400 1862400', 35),
+(2, 'Madonna', 575, 'Verenigde Staten', '+1 3425 185876', 64),
+(3, 'Taylor Swift', 570, 'Verenigde Staten', '+1 5876 236512', 33),
+(4, 'Beyoncé', 420, 'Verenigde Staten', '+1 6794 908465', 41),
+(5, 'Jennifer Lopez', 400, 'Verenigde Staten', '+1 3131 857345', 53);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
