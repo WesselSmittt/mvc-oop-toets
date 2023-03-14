d21b5ddd08fad331bd1eb2d56c31d5c313a67e5d
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2023-03-14 11:36:42 +0100

    Initial commit

---
+++ app/db/create_script.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 14 maart 2023 om 11:00
+-- Serverversie: 8.0.32
+-- PHP-versie: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `mvc-oop-toets` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `mvc-oop-toets`;
+
+DROP TABLE IF EXISTS `Zangeres`;
+CREATE TABLE IF NOT EXISTS `Zangeres` (
+  `Id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Naam` varchar(100) NOT NULL,
+  `NettoWaarde` smallint(100) NOT NULL,
+  `Land` varchar(100) NOT NULL,
+  `Mobiel` varchar(20) NOT NULL,
+  `Leeftijd` tinyint(3) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `Zangeres` (`Id`, `Naam`, `NettoWaarde`, `Land`, `Mobiel`, `Leeftijd`) VALUES
+(1, 'Rihanna',        '1400',   'Barbados',         '+1246 2400 1862400', 35),
+(2, 'Madonna',        '575',    'Verenigde Staten', '+1 3425 185876', 64),
+(3, 'Taylor Swift',   '570',    'Verenigde Staten', '+1 5876 236512', 33),
+(4, 'Beyoncé',        '420',    'Verenigde Staten', '+1 6794 908465', 41),
+(5, 'Jennifer Lopez', '400',    'Verenigde Staten', '+1 3131 857345', 53);
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ app/views/Homepage/index.php
@@ -0,0 +1,3 @@
+<h3><?= $data['title']; ?></h3>
+<a href="http://mvc-2209a.org/country/index">index</a><br>
+<a href="http://mvc-2209a.org/country/getcountries">Alle landen van de wereld</a>
